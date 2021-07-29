cd425a7c3ddf1d1116e273fcc825f0ebb6ba0b21
michael-pessoni <pessoni94@gmail.com>
2021-07-29 09:38:20 -0300

    Create Add New Business Card Activity and its layout

---
+++ app/src/main/java/com/michael/mybusinesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,14 @@
+package com.michael.mybusinesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.michael.mybusinesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
